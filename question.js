992497aa6e145457a969f165e2bbc7b0268d5526
Prateek Singh <prateeksingh899184@gmail.com>
2022-12-01 10:04:35 +0530

    code added

---
+++ question.js
@@ -0,0 +1,13 @@
+Hey Suraj 
+// code for checking prime no N 
+let c = 0;
+for(let i =0 ; i<=N ; i++) {
+ if((N%i)==0) {
+c++;
+}
+if(c==2){
+console.log("Prime");
+}
+else {
+console.log("Not Prime");
+}
