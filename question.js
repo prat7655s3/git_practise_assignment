a604fef25eda44bc9eb16acf84a330a7f0792000
Prateek Singh <prateeksingh899184@gmail.com>
2022-12-01 20:16:03 +0530

    Completed Done

--- question.js
+++ question.js
@@ -11,3 +11,17 @@ console.log("Prime");
 else {
 console.log("Not Prime");
 }
+// for Palindrome String str
+let bag = "" ;
+for(let i= (str.length)-1 ; i>=0 ; i++ ) 
+{
+bag = str[i] + bag ;
+}
+if(bag == str ) {
+console.log("Palindrome String ");
+}
+else {
+console.log("Not Palindrome String ");
+}
+
+
